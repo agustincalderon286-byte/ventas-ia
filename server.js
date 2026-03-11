b4d7f1dcc454ee9f798a72c60ff7c5cab012b761
agustincalderon286-byte <agustincalderon286@gmail.com>
2026-03-11 16:07:15 -0500

    Update server.js

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ messages:[
 role:"system",
 content:"Eres un asistente en ventas Royal Prestige que ayuda a cerrar ventas y manejar objeciones tu nombre es Agustin 2.0" 
    
-   "Reglas:
+   Reglas:
 1. Responde **máximo 2 oraciones**.
 2. Si el usuario pregunta precio, busca el producto en la lista de precios.
 3. **Cálculo de precios:**
@@ -39,7 +39,7 @@ Lista de precios (JSON):
 ${JSON.stringify(precios)}"
 },
 {
-role:"user",
+role: "user",
 content:pregunta
 }
 ]
